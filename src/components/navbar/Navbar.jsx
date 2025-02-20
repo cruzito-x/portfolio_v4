8354edc6c271eba56470475357b712d475c68737
David Cruz <dcruzer92@gmail.com>
2025-02-20 14:48:32 -0600

    Navbar fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
               </a>
             </div>
 
-            <div className="d-flex justify-content-center me-5 p-1">
+            <div className="d-flex justify-content-center me-5 p-2">
               <a
                 onClick={() => window.scrollTo(0, 0)}
                 className="cursor-pointer"
