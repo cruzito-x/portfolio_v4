2cfc983cd1004d44d2649d14f901c82de781d227
David Cruz <dcruzer92@gmail.com>
2025-02-21 20:31:21 -0600

    About traduction is finished

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,14 +1,13 @@
 import profilePhoto from "../../assets/img/me.png";
 
-const About = () => {
+const About = ({ lang }) => {
   return (
     <div className="container p-5 h-100" id="about">
       <div className="row mt-5 align-items-center">
         <div className="text-black text-center">
-          <h1 className="fw-semibold">About Me</h1>
+          <h1 className="fw-semibold">{lang.about?.text}</h1>
           <label className="fw-semibold enphasis-text-dark">
-            {" "}
-            Who&apos;s David Cruz?{" "}
+            {lang.about_subtitle?.text}
           </label>
         </div>
         <div className="col-lg-6 col-sm-12 mt-5">
@@ -20,48 +19,51 @@ const About = () => {
         </div>
         <div className="col-lg-6 col-sm-12">
           <p className="text-black mt-3">
-            I am currently 22 years old, I am a graduate student of
+            {lang.about_description?.text}
             <span className="fw-semibold enphasis-text">
-              {" "}
-              Systems and Computer Engineering
+              {lang.about_description_title?.text}
             </span>
-            , I have experience as{" "}
-            <span className="fw-semibold enphasis-text">Web Designer</span>{" "}
-            using design tools such as Figma and as{" "}
-            <span className="fw-semibold enphasis-text">Software Engineer</span>{" "}
-            I have experience in web application development using
-            JavaScript-based technologies such as React.js, Node.js and
-            Express.js, and Php-based such as Laravel, I also handle various
-            database engines such as MySQL, SQL Server and PostgreSQL for{" "}
-            <span className="fw-semibold enphasis-text">development</span> and{" "}
-            <span className="fw-semibold enphasis-text">management</span> of{" "}
+            {lang.about_experience_backfill?.text}
             <span className="fw-semibold enphasis-text">
-              relational database
+              {lang.about_secondary_title?.text}
             </span>
-            .
-          </p>
-
-          <p className="text-black">
-            I am currently pursuing a pre-specialty in{" "}
+            {lang.about_web_designer_title?.text}
             <span className="fw-semibold enphasis-text">
-              Cybersecurity Engineering
+              {lang.title?.text}
+            </span>
+            {lang.about_experience_description_backfill?.text}
+            <span className="fw-semibold enphasis-text">
+              {lang.development?.text}
             </span>{" "}
-            at the{" "}
+            {lang.and?.text}
             <span className="fw-semibold enphasis-text">
-              Universidad Tecnológica de El Salvador
+              {lang.management?.text}
+            </span>{" "}
+            {lang.of?.text}
+            <span className="fw-semibold enphasis-text">
+              {lang.relational_databases?.text}
             </span>
             .
           </p>
 
           <p className="text-black">
-            Beyond my professional life, I am also an{" "}
+            {lang.currently_preespeciality?.text}
             <span className="fw-semibold enphasis-text">
-              occasional cosplayer
+              {lang.cybersecurity_especiality?.text}
             </span>
-            , a creative passion that allows me to bring my favorite characters
-            to life through crafting and design. This hobby enhances my
-            creativity and problem-solving skills, which I also bring to my work
-            as an engineer.
+            {lang.at_the?.text}
+            <span className="fw-semibold enphasis-text">
+              {lang.university?.text}
+            </span>
+            .
+          </p>
+
+          <p className="text-black">
+            {lang.cosplayer_text?.text}
+            <span className="fw-semibold enphasis-text">
+              {lang.cosplayer_title?.text}
+            </span>
+            {lang.cosplay_description?.text}
           </p>
         </div>
       </div>
