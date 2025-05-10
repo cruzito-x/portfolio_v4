43f92378679ad2092b7d576e5ee4d1818a06cb97
David Cruz <dcruzer92@gmail.com>
2025-05-10 13:40:47 -0600

    Experience updated

--- src/views/experience/Experience.jsx
+++ src/views/experience/Experience.jsx
@@ -6,11 +6,12 @@ import assistantLogo from "../../assets/img/icons/assistant.png";
 import webLogo from "../../assets/img/icons/web.png";
 import preSpecialtyLottie from "../../assets/img/lottie/pre-specialty.json";
 import "./styles/experience.css";
-import { useEffect } from "react";
+import { use, useEffect } from "react";
 import { useState } from "react";
 
 const Experience = ({ lang }) => {
   const [showLottie, setShowLottie] = useState(false);
+  const [activeTab, setActiveTab] = useState("Laboral");
 
   useEffect(() => {
     if ("requestIdleCallback" in window) {
@@ -28,79 +29,111 @@ const Experience = ({ lang }) => {
           {lang.experience_subtitle?.text}
         </label>
       </div>
-      <div className="row mt-5">
+      <div className="row mt-5 align-items-center">
         <div className="col-lg-6 col-sm-12 d-flex align-items-center">
           <div className="timeline w-100 mb-5">
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={pythonLogo} alt="Python Logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">
-                  {lang.pre_specialty_time?.text}
-                </label>
-                <h3>{lang.pre_specialty_title?.text}</h3>
-                <p>{lang.university?.text}. - El Salvador.</p>
-              </div>
+            <div className="d-flex w-100 justify-content-center align-items-center mb-3">
+              <button
+                className={`btn me-1 fw-semibold fs-6 border-0 ${
+                  activeTab === lang.laboral_title?.text
+                    ? "enphasis-text"
+                    : "enphasis-text-dark"
+                }`}
+                onClick={() => setActiveTab(lang.laboral_title?.text)}
+              >
+                {lang.laboral_title?.text}
+              </button>
+              <button
+                className={`btn ms-1 fw-semibold fs-6 border-0 ${
+                  activeTab === lang.education_title?.text
+                    ? "enphasis-text"
+                    : "enphasis-text-dark"
+                }`}
+                onClick={() => setActiveTab(lang.education_title?.text)}
+              >
+                {lang.education_title?.text}
+              </button>
             </div>
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={webLogo} alt="Buy me a Coffee Logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">
-                  {lang.sms_time?.text}
-                </label>
-                <h3>{lang.sms_job?.text}</h3>
-                <p>Software Mobile Solutions S.A de C.V. - El Salvador.</p>
-              </div>
-            </div>
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={buyMeACoffeeLogo} alt="Buy me a Coffee Logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">
-                  {lang.tec101_time?.text}
-                </label>
-                <h3>{lang.tec101_job?.text}</h3>
-                <p>Tecnologías 101 S.A de C.V. - El Salvador.</p>
-              </div>
-            </div>
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={buyMeACoffeeLogo} alt="Buy me a Coffee Logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">
-                  {lang.codeEye_time?.text}
-                </label>
-                <h3>{lang.codeEye_job?.text}</h3>
-                <p>CodeEye Solutions Inc. - Canadá / El Salvador.</p>
-              </div>
-            </div>
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={htmxLogo} alt="HTMX logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">2020 - 2024</label>
-                <h3>{lang.about_description_title?.text}.</h3>
-                <p>{lang.university?.text}. - El Salvador.</p>
-              </div>
-            </div>
-            <div className="timeline-item d-flex align-items-center">
-              <div className="icon">
-                <img src={assistantLogo} alt="Google Assistant logo" />
-              </div>
-              <div className="timeline-content w-100">
-                <label className="fw-semibold text-white">
-                  {lang.mutual_fund_time?.text}
-                </label>
-                <h3>{lang.mutual_fund_job?.text}</h3>
-                <p>{lang.mutual_fund?.text} - El Salvador.</p>
-              </div>
-            </div>
+            {activeTab === lang.laboral_title?.text && (
+              <>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={webLogo} alt="Web Logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      {lang.sms_time?.text}
+                    </label>
+                    <h3>{lang.sms_job?.text}</h3>
+                    <p>Software Mobile Solutions S.A de C.V. - El Salvador.</p>
+                  </div>
+                </div>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={buyMeACoffeeLogo} alt="Buy me a Coffee Logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      {lang.tec101_time?.text}
+                    </label>
+                    <h3>{lang.tec101_job?.text}</h3>
+                    <p>Tecnologías 101 S.A de C.V. - El Salvador.</p>
+                  </div>
+                </div>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={buyMeACoffeeLogo} alt="Buy me a Coffee Logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      {lang.codeEye_time?.text}
+                    </label>
+                    <h3>{lang.codeEye_job?.text}</h3>
+                    <p>CodeEye Solutions Inc. - Canadá / El Salvador.</p>
+                  </div>
+                </div>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={assistantLogo} alt="Google Assistant logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      {lang.mutual_fund_time?.text}
+                    </label>
+                    <h3>{lang.mutual_fund_job?.text}</h3>
+                    <p>{lang.mutual_fund?.text} - El Salvador.</p>
+                  </div>
+                </div>
+              </>
+            )}
+            {activeTab === lang.education_title?.text && (
+              <>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={pythonLogo} alt="Python Logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      {lang.pre_specialty_time?.text}
+                    </label>
+                    <h3>{lang.pre_specialty_title?.text}</h3>
+                    <p>{lang.university?.text}. - El Salvador.</p>
+                  </div>
+                </div>
+                <div className="timeline-item d-flex align-items-center">
+                  <div className="icon">
+                    <img src={htmxLogo} alt="HTMX logo" />
+                  </div>
+                  <div className="timeline-content w-100">
+                    <label className="fw-semibold text-white">
+                      2020 - 2024
+                    </label>
+                    <h3>{lang.about_description_title?.text}.</h3>
+                    <p>{lang.university?.text}. - El Salvador.</p>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
         <div className="col-lg-6 col-sm-12 d-flex justify-content-center lottie-container">
